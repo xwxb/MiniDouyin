b4f61077b6a4542b9406b5da01d24c7c212ca55b
ZeeJoww <zeejoww@foxmail.com>
2023-02-11 11:39:21 +0800

    feat: 新增关注操作

--- controller/relation.go
+++ controller/relation.go
@@ -16,23 +16,37 @@ type UserListResponse struct {
 
 // RelationAction no practical effect, just check if token is valid
 func RelationAction(c *gin.Context) {
-	token := c.Query("token")
+	userId := c.MustGet("authUserObj").(*dao.TableUser).Id
+
 	toUserId, err := strconv.ParseInt(c.Query("to_user_id"), 10, 64)
 	if err != nil {
-		err = nil
+		c.JSON(http.StatusOK, Response{StatusCode: 1, StatusMsg: err.Error()})
+		return
 	}
-	userId, err := strconv.ParseInt(c.Query("user_id"), 10, 64)
-	if err != nil {
-		err = nil
-	}
-	fmt.Printf("to_user_id = %d, userId = %d\n", toUserId, userId)
 
-	ok := (len(token) > 0)
+	actionType, err := strconv.ParseInt(c.Query("action_type"), 10, 64)
+	if err != nil {
+		c.JSON(http.StatusOK, Response{StatusCode: 1, StatusMsg: err.Error()})
+		return
+	}
+
+	var ok bool
+	var msg string
+
+	if actionType == 1 {
+		ok, err = dao.UpFollow(toUserId, userId)
+		msg = "Already followed"
+	} else {
+		ok, err = dao.Unfollow(toUserId, userId)
+		msg = "Already unfollowed"
+	}
 
 	if ok {
 		c.JSON(http.StatusOK, Response{StatusCode: 0})
+	} else if err != nil {
+		c.JSON(http.StatusOK, Response{StatusCode: 1, StatusMsg: err.Error()})
 	} else {
-		c.JSON(http.StatusOK, Response{StatusCode: 1, StatusMsg: "User doesn't exist"})
+		c.JSON(http.StatusOK, Response{StatusCode: 1, StatusMsg: msg})
 	}
 }
 
@@ -73,7 +87,7 @@ func FollowerList(c *gin.Context) {
 	fmt.Printf("c: %v\n", c)
 }
 
-// beta: same with follower list
+// the same with follower list
 func FriendList(c *gin.Context) {
 	FollowerList(c)
 }
