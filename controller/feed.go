fa0d32f986a09b3d0f972a1564a25dfd549d4a6e
XBan <1205254918@qq.com>
2023-02-11 15:50:50 +0800

    feat: 正常返回视频流，有倒序和30个视频限制的特性

--- controller/feed.go
+++ controller/feed.go
@@ -1,14 +1,19 @@
 package controller
 
 import (
-	"github.com/gin-gonic/gin"
 	"net/http"
 	"time"
-	
-	_"gorm.io/driver/mysql"
-	_"gorm.io/gorm"
-)
 
+	"github.com/gin-gonic/gin"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+
+	"github.com/xwxb/MiniDouyin/config"
+	_"github.com/xwxb/MiniDouyin/dao"
+
+	_"fmt"
+)
 
 type FeedResponse struct {
 	Response
@@ -20,58 +25,41 @@ func (Video) TableName() string {
 	return "video"
 }
 
+func (User) TableName() string {
+	return "user"
+}
+
 // Feed same demo video list for every request
 func Feed(c *gin.Context) {
+	// 返回格式参考
+	// c.JSON(http.StatusOK, FeedResponse{
+	// 	Response:  Response{StatusCode: 0},
+	// 	VideoList: DemoVideos,
+	// 	NextTime:  time.Now().Unix(),
+	// })
+	
+
+	
+	// 数据库连接 不确定是否需要 or 让 Dao.init() 代替
+	dsn := config.LoginHead + "?charset=utf8mb4&parseTime=True"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to connect database"})
+		return
+	}
+	
+
+	var videos []Video
+	if err := db.Preload("Author").Order("create_time desc").Limit(30).Find(&videos).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "视频查询出现问题"})
+		return
+	}
 
 	c.JSON(http.StatusOK, FeedResponse{
-		Response:  Response{StatusCode: 0},
-		VideoList: DemoVideos,
+		Response:  Response{StatusCode: 0, StatusMsg: "success"},
+		VideoList: videos,
 		NextTime:  time.Now().Unix(),
 	})
 
-	// resp, err := getFeed()
-
-	// if err != nil {
-	// 	panic("failed to get feed!")
-	// }
-
-	// c.JSON(http.StatusOK, resp)
 }
-
-
-// func getFeed() (FeedResponse, error) {
-// 	// 数据库连接
-// 	dsn := "${root:114514@tcp(47.94.10.223:3306)/mdy?charset=utf8mb4&parseTime=True"
-// 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-
-// 	if err != nil {
-// 		panic("failed to connect database")
-// 		return nil, err
-// 	}
-
-// 	db.AutoMigrate(&Video{})
-
-// 	// todo1 添加逻辑：用户投稿
-
-// 	// 查询逻辑
-// 	// todo2 判断用户是否登录
-
-// 	// 如果未登录，根据视频id查询作者信息
-	
-
-// 	//组装返回
-
-// 	var videos []Video
-
-// 	if err := db.Preload("Author").Find(&videos).Error; err != nil {
-// 		return nil, err
-// 	}
-
-// 	resp := FeedResponse{
-// 		Response:  Response{StatusCode: 0},
-// 		VideoList: []Video{videos},
-// 		NextTime:  time.Now().Unix(),
-// 	}
-
-// 	return resp, nil
-// }
