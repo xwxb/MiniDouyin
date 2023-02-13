6aae512361e4ff7ffc8f83489e2a87c1659a2810
XBan <1205254918@qq.com>
2023-02-13 15:53:45 +0800

    冲突太复杂，准备本地把main合并到feed再合并回主分支

--- service/feed/feed_test.go
+++ service/feed/feed_test.go
@@ -8,11 +8,11 @@ import (
 )
 
 func TestGetFeed(t *testing.T) {
-	feed, _ := GetFeed(time.Now())
+	_, feed, _ := GetFeed(time.Now())
 	fmt.Println(jsonUtils.MapToJson(feed))
 }
 
 func TestGetFeedByUserId(t *testing.T) {
-	feed, _ := GetFeedByUserId(time.Now(), 1)
+	_, feed, _ := GetFeedByUserId(time.Now(), 1)
 	fmt.Println(jsonUtils.MapToJson(feed))
 }
