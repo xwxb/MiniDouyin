3f7c4c42514f66b5104b61f6ecffb849529efbc7
文骏 陈 <1635201934@qq.com>
2023-02-18 14:48:52 +0800

    hotfix:user表增加数值字段的增加和减少方法

--- dao/userDao.go
+++ dao/userDao.go
@@ -1,16 +1,24 @@
 package dao
 
 import (
+	"errors"
+	"gorm.io/gorm"
 	"log"
 )
 
 type TableUser struct {
-	Id            int64  `gorm:"primary_key;AUTO_INCREMENT" json:"id,omitempty"`
-	UserName      string `gorm:"column:user_name" json:"name,omitempty"`
-	Password      string `gorm:"column:password" json:"-"` // 转化为json格式的时候自动忽略password字段
-	FollowCount   int64  `gorm:"column:follow_count" json:"follow_count"`
-	FollowerCount int64  `gorm:"column:follower_count" json:"follower_count,omitempty"`
-	IsFollow      bool   `gorm:"-"`
+	Id              int64  `gorm:"primary_key;AUTO_INCREMENT" json:"id,omitempty"`
+	UserName        string `gorm:"column:user_name" json:"name,omitempty"`
+	Password        string `gorm:"column:password" json:"-"` // 转化为json格式的时候自动忽略password字段
+	FollowCount     int64  `gorm:"column:follow_count" json:"follow_count"`
+	FollowerCount   int64  `gorm:"column:follower_count" json:"follower_count,omitempty"`
+	IsFollow        bool   `gorm:"-" json:"is_follow"`
+	Avatar          string `gorm:"avatar" json:"avatar"`
+	BackGroundImage string `gorm:"background_image" json:"background_image"`
+	Signature       string `gorm:"signature" json:"signature"`
+	TotalFavorited  int    `gorm:"total_favorited" json:"total_favorited"`
+	WorkCount       int    `gorm:"work_count" json:"work_count"`
+	FavoriteCount   int    `gorm:"favorite_count" json:"favorite_count"`
 }
 
 type User struct {
@@ -21,7 +29,6 @@ type User struct {
 	FollowerCount int64  `json:"follower_count,omitempty" gorm:"column:follower_count"`
 	IsFollow      bool   `json:"is_follow,omitempty" gorm:"-"`
 }
-
 
 func (user TableUser) TableName() string {
 	return "user"
@@ -53,6 +60,43 @@ func GetUserByUserId(id int64) (TableUser, error) {
 		return user, err
 	}
 	return user, nil
+}
+
+// UpdateUserByNum
+//
+//	@Description: 更新 User 的数字字段（包括视频数量，赞数量，获赞数量等等）
+//	@param userId
+//	@param column 字段名字
+//	@param oper 操作类型：以点赞为例为 1， 取消赞为 -1
+//	@return bool
+func UpdateUserByNum(userId int64, column string, oper int64) error {
+	// 如果是增加字段，直接执行
+	if oper == 1 {
+		if err := Db.Model(&TableUser{Id: userId}).Update(column, gorm.Expr(column+"+ ?", oper)).Error; err != nil {
+			log.Printf("更新%v的%v失败", userId, column)
+			return err
+		}
+		return nil
+	}
+
+	// 如果是减少字段，那么需要开启事务，防止减少后数量小于 0。
+	// 开启事务后，先减少字段的数，然后判断该字段是否小于 0。
+	tx := Db.Begin()
+	if err := tx.Model(&TableUser{Id: userId}).Update(column, gorm.Expr(column+"+ ?", oper)).Error; err != nil {
+		log.Printf("更新%v的%v失败", userId, column)
+		tx.Rollback()
+		return err
+	}
+
+	var num int64
+
+	if tx.Table("user").Where("id = ? ", userId).Select(column).Scan(&num); num < 0 {
+		tx.Rollback()
+		return errors.New("不能少于0")
+	}
+
+	tx.Commit()
+	return nil
 }
 
 func InsertUser(user *TableUser) bool {
---
+++ service/user/user_test.go
@@ -0,0 +1,78 @@
+package user
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestAddFavoriteCount(t *testing.T) {
+	if err := AddFavoriteCount(1); err != nil {
+		fmt.Println("增加favorite_count失败")
+		fmt.Println(err)
+	}
+	fmt.Println("增加favorite_count成功")
+}
+
+func TestSubFavoriteCount(t *testing.T) {
+	if err := SubFavoriteCount(1); err != nil {
+		fmt.Println("减少favorite_count失败")
+		fmt.Println(err)
+	}
+	fmt.Println("减少favorite_count成功")
+}
+
+func TestAddFollowCount(t *testing.T) {
+	if err := AddFollowCount(1); err != nil {
+		fmt.Println("增加follow_count失败")
+		fmt.Println(err)
+	}
+	fmt.Println("增加follow_count成功")
+}
+
+func TestSubFollowCount(t *testing.T) {
+	if err := SubFollowCount(1); err != nil {
+		fmt.Println("减少follow_count失败")
+		fmt.Println(err)
+	}
+	fmt.Println("减少follow_count成功")
+}
+
+func TestAddFollowerCount(t *testing.T) {
+	if err := AddFollowerCount(1); err != nil {
+		fmt.Println("增加follower_count失败")
+		fmt.Println(err)
+	}
+	fmt.Println("增加follower_count成功")
+}
+
+func TestSubFollowerCount(t *testing.T) {
+	if err := SubFollowerCount(3); err != nil {
+		fmt.Println("减少follower_count失败")
+		fmt.Println(err)
+	}
+	fmt.Println("减少follower_count成功")
+}
+
+func TestAddWorkCount(t *testing.T) {
+	if err := AddWorkCount(1); err != nil {
+		fmt.Println("增加work_count失败")
+		fmt.Println(err)
+	}
+	fmt.Println("增加work_count成功")
+}
+
+func TestAddTotalFavorite(t *testing.T) {
+	if err := AddFollowerCount(1); err != nil {
+		fmt.Println("增加total_favorite失败")
+		fmt.Println(err)
+	}
+	fmt.Println("增加total_favorite成功")
+}
+
+func TestSubTotalFavorite(t *testing.T) {
+	if err := SubFollowerCount(1); err != nil {
+		fmt.Println("减少total_favorite失败")
+		fmt.Println(err)
+	}
+	fmt.Println("减少total_favorite成功")
+}
