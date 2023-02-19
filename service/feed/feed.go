e8c36d1a4e41e33b2ccd7793ec317faf92d995c6
文骏 陈 <1635201934@qq.com>
2023-02-19 22:07:50 +0800

    feat:注册用户加上默认头像和背景图，优化feed流title

--- service/feed/feed.go
+++ service/feed/feed.go
@@ -2,10 +2,9 @@ package feed
 
 import (
 	"encoding/json"
+	"github.com/xwxb/MiniDouyin/dao"
 	"log"
 	"time"
-	"github.com/xwxb/MiniDouyin/dao"
-	
 )
 
 func GetFeed(latestTime time.Time) (time.Time, []dao.Video, error) {
@@ -16,7 +15,6 @@ func GetFeed(latestTime time.Time) (time.Time, []dao.Video, error) {
 		log.Printf("err = %v\n", err)
 	}
 
-	
 	jsonErr := json.Unmarshal([]byte(VideoList), &FeedList)
 	if jsonErr != nil || len(FeedList) == 0 {
 		log.Println("解码错误或数据库已空")
@@ -26,7 +24,7 @@ func GetFeed(latestTime time.Time) (time.Time, []dao.Video, error) {
 	//fmt.Printf("未登入获取的feed流：%v\n", FeedList)
 	// log.Printf("当前获取到的视频数%v\n", len(FeedList))
 
-	return FeedList[len(FeedList) - 1].CreatedAt, FeedList, nil
+	return FeedList[len(FeedList)-1].CreatedAt, FeedList, nil
 }
 
 func GetFeedByUserId(latestTime time.Time, userId int64) (time.Time, []dao.Video, error) {
@@ -50,5 +48,5 @@ func GetFeedByUserId(latestTime time.Time, userId int64) (time.Time, []dao.Video
 		FeedList[k].Author.IsFollow, _ = dao.IsFollowed(userId, v.Author.Id)
 	}
 	//fmt.Printf("登入获取的feed流：%v\n", FeedList)
-	return FeedList[len(FeedList) - 1].CreatedAt, FeedList, nil
+	return FeedList[len(FeedList)-1].CreatedAt, FeedList, nil
 }
