96af0efb0c39dcfc10a4130e55351c3406a17dcb
XBan <1205254918@qq.com>
2023-02-24 23:15:19 +0800

    feat: 测试相关更改和README.MD

--- main.go
+++ main.go
@@ -5,9 +5,34 @@ import (
 	"github.com/xwxb/MiniDouyin/middleware/cos"
 	"github.com/xwxb/MiniDouyin/service"
 	"github.com/xwxb/MiniDouyin/utils/directoryUtils"
+
+	// //测试相关包
+	"log"
+    "net/http"
+    _ "net/http/pprof"//自动注册到本程序监听的端口
+	// "os"
+    // "runtime/pprof"
 )
 
 func main() {
+	// pprof
+	go func() {
+        log.Println(http.ListenAndServe("localhost:6060", nil))
+    }()
+
+	// f, err := os.Create("cpu.pprof")
+    // if err != nil {
+    //     log.Fatal(err)
+    // }
+    // defer f.Close()
+
+    // err = pprof.StartCPUProfile(f)
+    // if err != nil {
+    //     log.Fatal(err)
+    // }
+    // defer pprof.StopCPUProfile()
+
+	// my application
 	go service.RunMessageServer()
 	go cos.UploadHandle()
 	// 每10秒扫描一次public目录，删除空文件夹
